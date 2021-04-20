1a67a166adc7e492d1f33079ed8601b5d78c7049
Roman Pikulenko <pikulenkor@gmail.com>
2021-04-21 06:57:28 +1000

    update2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "12345";
+            this.label1.Text = "23456_save2";
             // 
             // Form1
             // 
